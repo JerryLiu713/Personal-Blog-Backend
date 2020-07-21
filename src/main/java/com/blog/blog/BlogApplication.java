8edea6fb98551d3b406b5ff7dc6a34504b5fe916
Jerry Liu <jerrypersonallaptop@Jerrys-MacBook-Pro.local>
2020-07-21 12:23:45 -0700

    encrypt the database information

--- src/main/java/com/blog/blog/BlogApplication.java
+++ src/main/java/com/blog/blog/BlogApplication.java
@@ -1,9 +1,11 @@
 package com.blog.blog;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class BlogApplication {
 
 	public static void main(String[] args) {
---
+++ src/test/java/com/blog/blog/JasyptTest.java
@@ -0,0 +1,23 @@
+package com.blog.blog;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentPBEConfig;
+import org.junit.jupiter.api.Test;
+
+public class JasyptTest {
+    @Test
+    public void testEncrypt() throws Exception {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        EnvironmentPBEConfig config = new EnvironmentPBEConfig();
+
+
+    }
+
+    @Test
+    public void testDe() throws Exception {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        EnvironmentPBEConfig config = new EnvironmentPBEConfig();
+
+
+    }
+}
